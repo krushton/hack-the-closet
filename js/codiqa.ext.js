babd92065ab0cb2fae5121245dbf76c5a0ab401e
Sandra Helsley <syh@ischool.berkeley.edu>
2013-07-13 09:12:30 -0700

    fixed js and css ref issue

--- js/codiqa.ext.js
+++ js/codiqa.ext.js
@@ -1,1 +1,14 @@
-// Put your custom code here
+$(document).ready(function(){
+
+	$("#tag-btn").on("click",function(event){
+
+		$.get("http://vwala.pythonanywhere.com/items/" + $('input[id=tag-num]').val(), function(data, textStatus, jqXHR){
+			$("#item").removeClass("hidden");
+
+			
+
+		});
+
+	});
+
+})
